18a2a642d5b655c54a4e2e129ad0684063f51c07
omargaer <omarololoshkin@gmail.com>
2024-10-24 21:18:07 +1000

    init

---
+++ DockPanel3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_first_app
+{
+    /// <summary>
+    /// Interaction logic for DockPanel3.xaml
+    /// </summary>
+    public partial class DockPanel3 : Window
+    {
+        public DockPanel3()
+        {
+            InitializeComponent();
+        }
+    }
+}
